5799c2ae0d1241ba08ee0992c88ca66c9cea2801
Benjamin Bours <boursbenjamin@gmail.com>
2024-01-03 11:39:05 +0000

    Update page title

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,4 +1,10 @@
 import MainApp from './MainApp';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Composite - The game',
+    description: 'TODO: Add description',
+};
 
 export default async function Home() {
     return (
