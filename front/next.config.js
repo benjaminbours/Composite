d3c5218eed4efb5cf716fa7141e98ec0221b8747
Benjamin Bours <boursbenjamin@gmail.com>
2024-01-11 10:27:15 +0000

    Add sentry

--- front/next.config.js
+++ front/next.config.js
@@ -3,6 +3,7 @@ const version = require('./package.json').version;
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
+const { withSentryConfig } = require('@sentry/nextjs');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -20,4 +21,48 @@ const nextConfig = {
     // productionBrowserSourceMaps: true,
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+// Injected content via Sentry wizard below
+
+let config = withBundleAnalyzer(nextConfig);
+
+if (process.env.NEXT_PUBLIC_STAGE !== 'development') {
+    config = withSentryConfig(
+        config,
+        {
+            // For all available options, see:
+            // https://github.com/getsentry/sentry-webpack-plugin#options
+
+            // Suppresses source map uploading logs during build
+            silent: true,
+            org: 'benjamin-bours',
+            project: 'composite',
+        },
+        {
+            // For all available options, see:
+            // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+            // Upload a larger set of source maps for prettier stack traces (increases build time)
+            widenClientFileUpload: true,
+
+            // Transpiles SDK to be compatible with IE11 (increases bundle size)
+            transpileClientSDK: true,
+
+            // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+            tunnelRoute: '/monitoring',
+
+            // Hides source maps from generated client bundles
+            hideSourceMaps: true,
+
+            // Automatically tree-shake Sentry logger statements to reduce bundle size
+            disableLogger: true,
+
+            // Enables automatic instrumentation of Vercel Cron Monitors.
+            // See the following for more information:
+            // https://docs.sentry.io/product/crons/
+            // https://vercel.com/docs/cron-jobs
+            automaticVercelMonitors: true,
+        },
+    );
+}
+
+module.exports = config;
