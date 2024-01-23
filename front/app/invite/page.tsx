2100177e02b78f4fc91e411d4a92dd3ca58c5b20
Benjamin Bours <boursbenjamin@gmail.com>
2024-01-23 13:10:38 +0000

    Address build issue

--- front/app/invite/page.tsx
+++ front/app/invite/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Suspense } from 'react';
 import { InvitePage } from './InvitePage';
 
 export const metadata: Metadata = {
@@ -8,5 +9,11 @@ export const metadata: Metadata = {
 };
 
 export default function Invite() {
-    return <InvitePage />;
+    return (
+        <>
+            <Suspense>
+                <InvitePage />
+            </Suspense>
+        </>
+    );
 }
