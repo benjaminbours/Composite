e18bae8890359432a4d6f1d589ac328d7d0d43c7
Benjamin Bours <boursbenjamin@gmail.com>
2024-01-29 13:16:21 +0000

    Display bounce arrow helper only when player is inside

--- front/app/Game/elements/SkinBounce.ts
+++ front/app/Game/elements/SkinBounce.ts
@@ -3,9 +3,11 @@ import { ElementToBounce, Side } from '@benjaminbours/composite-core';
 
 export class SkinBounce extends Object3D {
     public directionHelper: ArrowHelper;
+    public isPlayerInside = false;
 
     constructor(public bounce: ElementToBounce) {
         super();
+        this.name = `skin-bounce-${bounce.bounceID}`;
 
         const dir = new Vector3(0, 0, -1);
         dir.normalize();
@@ -14,6 +16,8 @@ export class SkinBounce extends Object3D {
         if (bounce.side === Side.SHADOW) {
             hex = 0x000000;
         }
+
+        // this arrow is added and removed from the scene depending if the player is inside or not
         this.directionHelper = new ArrowHelper(
             dir,
             this.bounce.center,
@@ -22,7 +26,6 @@ export class SkinBounce extends Object3D {
             25,
         );
         (this.directionHelper as any).collidable = false;
-        this.add(this.directionHelper);
         this.position.copy(bounce.position);
         this.rotation.copy(bounce.rotation);
     }
