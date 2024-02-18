246de0ddefbcd2742b1c55b6414a0864b502fdc5
Benjamin Bours <boursbenjamin@gmail.com>
2024-02-18 18:02:11 +0000

    Add api client package generated by openapi-generator, based on swagger spec

---
+++ packages/api-client/src/models/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './LoginDto';
+export * from './RegisterDto';
+export * from './ResetPasswordDto';
+export * from './TokensDto';
+export * from './UpdatePasswordDto';
