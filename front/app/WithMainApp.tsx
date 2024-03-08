ab6654eac21c40bfa7d72e8fc3e080eb64887fa8
Benjamin Bours <boursbenjamin@gmail.com>
2024-03-08 19:00:10 +0000

    Add retrieve session mechanism. Add function to setup services depending of execution context (client or server).

--- front/app/WithMainApp.tsx
+++ front/app/WithMainApp.tsx
@@ -1,19 +1,17 @@
 'use client';
 import 'reflect-metadata';
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { usePathname } from 'next/navigation';
 import { SnackbarProvider } from 'notistack';
 import { StoreProvider } from 'easy-peasy';
 import MainApp from './MainApp';
-import { configureServices, configureStore } from './core/frameworks';
+import { configureStore } from './core/frameworks';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Route } from './types';
+import { useStoreActions } from './hooks';
+import { setupProjectEnv } from './utils/setup';
 
-configureServices({
-    api: {
-        origin: process.env.NEXT_PUBLIC_BACKEND_URL!,
-    },
-});
+setupProjectEnv('client');
 
 const darkTheme = createTheme({
     palette: {
@@ -27,13 +25,25 @@ interface Props {
     children: React.ReactNode;
 }
 
+const WithRetrieveSession: React.FC<Props> = ({ children }) => {
+    const retrieveSession = useStoreActions(
+        (actions) => actions.user.retrieveSession,
+    );
+
+    useEffect(() => {
+        retrieveSession();
+    }, [retrieveSession]);
+
+    return children;
+};
+
 export const WithMainApp: React.FC<Props> = ({ children }) => {
     const path = usePathname();
 
     const content = useMemo(() => {
         if (
             path.includes(Route.TIMELINE) ||
-            path.includes(Route.LEVEL_EDITOR) ||
+            path.includes(Route.LEVEL_EDITOR_ROOT) ||
             path.includes(Route.LOGIN) ||
             path.includes(Route.REGISTER) ||
             path.includes(Route.SIGN_UP_EMAIL_ACTIVATED) ||
@@ -49,7 +59,9 @@ export const WithMainApp: React.FC<Props> = ({ children }) => {
     return (
         <ThemeProvider theme={darkTheme}>
             <SnackbarProvider>
-                <StoreProvider store={store}>{content}</StoreProvider>
+                <StoreProvider store={store}>
+                    <WithRetrieveSession>{content}</WithRetrieveSession>
+                </StoreProvider>
             </SnackbarProvider>
         </ThemeProvider>
     );
--- front/app/[lng]/page.tsx
+++ front/app/[lng]/page.tsx
@@ -6,6 +6,6 @@ export const metadata: Metadata = {
         "In this 2.5D platformer game, two players will embody the essence of light and shadow, with one player representing light and the other embodying shadow. The game's mechanics are designed to leverage the interplay between these two elements, creating a cooperative and immersive experience where your skills will be challenged.",
 };
 
-export default async function Home() {
+export default async function Home({ params: { lng } }: any) {
     return <></>;
 }
